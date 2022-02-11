7cdcc1042a0c81a63b6eada436e383bc96ca4fed
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-02-10 20:37:24 -0600

    Drew the ship to the screen

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ class Settings: #work with just one settings object any time we access one setti
 
     def __init__(self):
         self.screen_width = 1200 #screen settings
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230,230,230)
