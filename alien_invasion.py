928b277627b92125b32e81f1d942f5fce3cdcf99
“Aurelie” <aurelie_boutin1@baylor.edu>
2022-02-12 12:58:04 -0600

    Refactored run_game() method

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,7 @@ import pygame #importing modules
 from settings import Settings
 from ship import Ship
 
-class AlienInvasion: #manage game assets and behavior
+class AlienInvasion: #manage ressources and behavior
 
     def __init__(self): #method to initiate the game and create resources
         pygame.init() #initializes the background settings that Pygame needs to work properly
@@ -13,14 +13,15 @@ class AlienInvasion: #manage game assets and behavior
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) 
         #display window to draw graphical ele­ments
-        #(1200, 800) = tuple that defines the dimensions of the game window
+        #(1200, 800) = tuple = dimensions of the game window
         #attribute self.screen will be available in all methods in the class.
         #The object we assigned to self.screen = surface = a part of the screen where a game element can be displayed.
         #surface here = represents the entire game window.
         #use the screen_width and screen_height attributes of self.settings
-        pygame.display.set_caption("Alien Invasion") 
-
+        
         self.ship = Ship(self) #create an instance #self = current instance of AlienInvasion, gives Ship access to esources, screen object
+
+        pygame.display.set_caption("Alien Invasion") #window title
 
         self.bg_color = (230,230,230) #set background color
 
@@ -28,18 +29,21 @@ class AlienInvasion: #manage game assets and behavior
         # event = action the user performs = pressing a key or moving the mouse
         # listen for events and get the right output
         while True:
-            for event in pygame.event.get(): #runs at every keyboard or mouse action
+            self._check_events()
+            self._update_screen() #redraws screen every time you cycle
+    
+    def _check_events(self):
+        for event in pygame.event.get(): #runs at every keyboard or mouse action
             #returns a list of events that have taken place since the last time this function was called
-                if event.type == pygame.QUIT:
-                    sys.exit
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            self.screen.fill(self.settings.bg_color) #fill the screen after each loop
+    def _update_screen(self): #Update screen image and switch to new screen
+        self.screen.fill(self.settings.bg_color) #fill the screen after each loop
+        self.ship.blitme() #draw the ship
+        pygame.display.flip() #display the new positions of game elements, hides old ones
 
-            self.ship.blitme() #draw the ship
-            
-            pygame.display.flip() #display the new positions of game elements, hides old ones
-
-if __name__ == '__main__':
+if __name__ == '__main__': #check for special variables, which is set during program execution
     ai = AlienInvasion()
     ai.run_game() #create an instance
 
--- ship.py
+++ ship.py
@@ -7,9 +7,9 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect() #access the screen’s rect attribute & place the ship in the correct location on the screen.
         
         self.image = pygame.image.load('ship.bmp') # returns a surface representing the ship
-        self.rect = self.image.get_rect() #call get_rect() to access the ship surface’s rect attribute so we can later use it to place the ship
+        self.rect = self.image.get_rect() #get external rectangle of the ship 
 
-        self.rect.midbottom = self.screen_rect.midbottom #screen position where to start each ship
+        self.rect.midbottom = self.screen_rect.midbottom #where to position new ship
 
     def blitme(self):
         self.screen.blit(self.image, self.rect) #draw the ship at its current location
